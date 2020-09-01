985535e2dc008faceaa3b0e83841e94f5656e8aa
Thonny Cleuton <thonnycleuton@gmail.com>
2020-09-01 08:59:27 -0300

    Recuperação de Senha via e-mail

--- Plantoes/settings.py
+++ Plantoes/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '7&a+$&i0wdpn0=*k+j!q-hr#9lr_ss+*pfw(zq^3-(4l73(m^h'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Plantoes.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -115,9 +110,23 @@ USE_L10N = False
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
 DATE_INPUT_FORMATS = ['%d/%m/%Y']
+
+# E-mails
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'thonnycleuton@gmail.com'
+EMAIL_HOST_PASSWORD = 'R1cr0nd0p4.'
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+CONTACT_EMAIL = 'thonnycleuton@gmail.com'
+
+# Auth
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_URL = 'logout'
--- sorteio/urls.py
+++ sorteio/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetDoneView, \
+    PasswordResetConfirmView, PasswordResetCompleteView, LoginView, LogoutView
 
 from sorteio.views import *
 
@@ -10,4 +12,13 @@ urlpatterns = [
     url(r'^defensor_details/(?P<pk>\d+)/$', DefensorDetail.as_view(), name='defensor_detail'),
     url(r'feriados/', FeriadoList.as_view(), name='feriado_list'),
     url(r'afastamentos/', AfastamentoFormView.as_view(), name='afastamento_create'),
+
+    url(r'^password_change/$', PasswordChangeView.as_view(success_url=reverse_lazy('home'), template_name='registration/password_change.html'), name='password_change'),
+    url(r'^password_reset/$', PasswordResetView.as_view(email_template_name='registration/password_reset_email.html', template_name='registration/password_reset.html', ), name='password_reset'),
+    url(r'^password_reset/done/$', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(r'^reset/done/$', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    url(r'^entrar/$', LoginView.as_view(), name='login'),
+    url(r'^sair/$', LogoutView.as_view(), name='logout'),
 ]
