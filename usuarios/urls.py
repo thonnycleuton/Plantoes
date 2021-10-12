47b78e29be8c6a148e14c7c64b38dfad91930cc8
PedroHenriqueDevBR <p.lyma18@gmail.com>
2021-10-12 12:02:27 -0300

    feat: alteracao de senha do usuario logado

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     url(r'^entrar/$', LoginView.as_view(), name='login'),
     url(r'^sair/$', LogoutView.as_view(), name='logout'),
+    url(r'^senha/alterar$', ChangePasswordView.as_view(), name='change_password'),
 ]
